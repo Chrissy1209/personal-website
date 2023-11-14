36b6b9d1285ae1aa1e0a0f72d78db6d3f236d820
Chrissy1209 <chrissy93881209@gmail.com>
2023-11-14 19:01:36 +0800

    fix: width bug fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
       <header className={sx.container}>
-        <div className={sx.gradientText}>
+        <div className={{}}>
           <p>
             <i>Hi, I'm</i>
             <br />
