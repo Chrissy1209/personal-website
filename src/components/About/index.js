a776733e54514a250c07e6d5a402f6b5c021d456
Chrissy1209 <chrissy93881209@gmail.com>
2023-12-01 19:34:09 +0800

    feat: update item content

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,8 @@ import sx from "./About.module.sass";
 
 const links = {
   leadershipFigures: 'https://drive.google.com/file/d/1vh9lmS-SFPL-_R-JAsicbYHWX5nQ9IEi/view?usp=sharing',
-  qubic: 'https://www.qubic.market/?gclid=Cj0KCQiAgOefBhDgARIsAMhqXA6mrn45trkttgGmtAGN59SSA6HWlbLfn5A3wmMC872WLBDizE1n7ToaAqVEEALw_wcB'
+  qubic: 'https://www.qubic.market/?gclid=Cj0KCQiAgOefBhDgARIsAMhqXA6mrn45trkttgGmtAGN59SSA6HWlbLfn5A3wmMC872WLBDizE1n7ToaAqVEEALw_wcB',
+  graduationProject: 'https://drive.google.com/file/d/14gQrviCTfyf2MHIN3QFCgGyZLBIiEDeu/view?usp=share_link'
 };
 
 const itemData = [{
@@ -17,10 +18,10 @@ const itemData = [{
         In July 2023, I participated in a summer exchange program in Sweden, where I enrolled in a <strong>Leadership and Organizational Behavior</strong> course alongside nearly 30 individuals from 12 different countries.
       </p>
       <p>
-        I actively engaged a variety of activities and discussions, including treasure hunting and <strong>corporate role-playing</strong>. We assumed different roles within a company to gain insights into the internal decision-making process and the significance of <strong>communication</strong>, both within an organization and between companies.
+        During the program, I actively engaged in various hands-on activities that allowed me to apply theoretical knowledge to real-world scenarios. One notable instance was a <strong>corporate role-playing</strong>, where we assumed different roles within a company to simulate complex <strong>decision-making processes</strong> and enhance our <strong>problem-solving skills</strong>.
       </p>
       <p>
-        It was an incredibly rewarding experience. Being part of such a diverse class, I acquired valuable skills in <strong>interacting, communicating, and collaborating</strong> with people from vastly different cultures, and succeeded in a group project on <strong><a className={sx.link} href={links.leadershipFigures} target="_blank">Contemporary Leadership Figures</a></strong>.
+        Being part of such a diverse class, I acquired valuable skills in <strong>interacting, communicating, and collaborating</strong> with people from vastly different cultures, and excelled in a group project on <strong><a className={sx.link} href={links.leadershipFigures} target="_blank">Contemporary Leadership Figures</a></strong>.
       </p>
     </>
 }, {
@@ -31,7 +32,7 @@ const itemData = [{
   text:
     <>
       <p>
-        During my last year of college, I pursued a <strong>frontend/blockchain engineering</strong> internship at MaiCoin, attending daily stand-up, weekly one-on-one sessions, and solving problems through collaborative efforts.
+        Throughout my last year of college, I pursued a <strong>frontend/blockchain engineering</strong> internship at MaiCoin, engaging daily stand-ups, weekly one-on-one meetings, and solving problems through collaborative efforts.
       </p>
       <p>
         - - -
@@ -41,14 +42,15 @@ const itemData = [{
           Established an <strong>admin dashboard</strong> to support daily operations for <strong><a className={sx.link} href={links.qubic} target="_blank">Qubic</a></strong> (an NFT Wallet), managing KYC user list and push notifications using Next.js, TypeScript and Material-UI.
         </li>
         <li>
-          Built an <strong>NFT verification platform</strong> for users who want to redeem their NFT by Next.js, Firebase cloud function and GraphQL.
+          Built an <strong>NFT verification platform</strong> for users who want to redeem their NFT by Next.js, Firebase cloud function, and GraphQL.
         </li>
         <li>
-          Implemented two websites and two landing pages from concept to deployment, standardized outputs with <strong>responsive web design</strong> and <strong>mobile-first</strong> strategy.
+          Implemented two websites and two landing pages from concept to deployment, standardized outputs with <strong>responsive web design</strong>, and <strong>mobile-first</strong> strategy.
         </li>
       </ul>
       <p>
-        Eager to apply these experiences in future roles, I look forward to contributing my skills to dynamic projects and further advancing as a frontend engineer.
+        This journey proved to be incredibly rewarding. I thrived in an environment that fostered <strong>continuous learning</strong> and refined my <strong>technical proficiency</strong>. Eager to contribute these experiences to dynamic projects and further advance in future roles.
+        {/* It was an incredibly rewarding experience. Eager to apply these experiences in future roles, I look forward to contributing my skills to dynamic projects and further advancing as a frontend engineer. */}
       </p>
     </>
 }, {
@@ -65,7 +67,7 @@ const itemData = [{
         I actively engaged a variety of activities and discussions, including treasure hunting and <strong>corporate role-playing</strong>. I assumed different roles within a company to gain insights into the internal decision-making process and the significance of <strong>communication</strong>, both within an organization and between companies.
       </p>
       <p>
-        Being part of such a diverse class, I gained valuable skills in <strong>interacting, communicating, and collaborating</strong> with people from vastly different cultures, and I'm excited to apply these enriched perspectives to future challenges and opportunities.
+        Being part of such a diverse class, I gained valuable skills in <strong>interacting, communicating, and collaborating</strong> with people from vastly different cultures, and I'm excited to apply these enriched perspectives to future challenges and opportunities <strong><a className={sx.link} href={links.graduationProject} target="_blank">結案報告書</a></strong>.
       </p>
     </>
 }, {
